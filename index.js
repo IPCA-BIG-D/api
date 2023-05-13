29c5d8a952e571ef6c975af9d99a29ce54302775
Daniel <a24003@alunos.ipca.pt>
2023-05-13 01:41:43 +0100

    1

---
+++ index.js
@@ -0,0 +1,14 @@
+import  express  from 'express';
+import bodyParser from 'body-parser';
+import userRoutes from './routes/users.js'
+
+const app = express()
+const PORT = 5000;
+
+app.use(bodyParser.json())
+
+app.use('/users', userRoutes);
+
+app.get('/',(req,res)=>res.send('ola da pagina inicial'));
+
+app.listen(PORT,() => console.log(`server Running on port: http://localhost:${PORT}`));
